3f9668b9cf44cc9670f469d13ef5410b87ed17e8
ravichauhan8153 <r.jchauhan330@gmail.com>
2024-06-18 22:06:46 +0530

    fix

--- src/app/modules/dashboard/song-dashboard/song-dashboard.component.ts
+++ src/app/modules/dashboard/song-dashboard/song-dashboard.component.ts
@@ -11,4 +11,121 @@ import * as Highcharts from 'highcharts';
 })
 export class SongDashboardComponent {
 
+  constructor(
+    
+  )
+  {
+    
+  }
+
+  ngOnInit(): void { 
+    this.renderScatterChart();
+    this.renderBarChart();
+  }
+
+  renderScatterChart() {
+   
+    let chart2 = Highcharts.chart({
+      chart: {
+        renderTo: 'chart-1',
+        type: 'spline',
+        height: 276,
+      },
+
+      title: {
+        text: '',
+      },
+
+      credits: {
+        enabled: false,
+      },
+
+      xAxis: {
+        type: 'category',
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+
+      yAxis: {
+        title: {
+          text: 'Post Stats',
+        },
+        plotLines: [
+          {
+            value: 0,
+            width: 1,
+            color: '#808080',
+          },
+        ],
+      },
+
+      tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.y:.2f}',
+      },
+
+      legend: {
+        enabled: true,
+      },
+
+      exporting: {
+        enabled: false,
+      },
+
+      series: [
+        {
+          type: 'line',
+          name: 'Re post',
+          color: '#219653',
+          data: [10,20],
+        },
+        {
+          type: 'line',
+          name: 'Total Comment',
+          color: '#9B51E0',
+          data: [10,20],
+        },
+        {
+          type: 'line',
+          name: 'Total Like',
+          color: '#F2994A',
+          data: [10,20],
+        },
+      ],
+    });
+  }
+
+  renderBarChart(){
+    var chart3 = Highcharts.chart({
+      chart: {
+        renderTo: 'chart-2', // Specify the ID of the container where the chart should be rendered
+        type: 'column',
+      },
+      title: {
+        text: 'Week-wise Connection and Followers Data',
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+      yAxis: {
+        title: {
+          text: 'Follower-Connection',
+        },
+      },
+      series: [
+        {
+          name: 'Followers',
+          color: '#FB6514',
+          type: 'column',
+          data: [10,20],
+        },
+        {
+          name: 'Connection',
+          color: '#FEB273',
+          type: 'column',
+          data: [10,20],
+        },
+      ],
+    });
+  }
+
 }
